95f0a6de7e66790e831173a5c95a3750a38b2ac1
Takashi imai <takashi.imai.jp@gmail.com>
2020-01-13 09:31:12 +0900

    不具合

--- html/5de1c9089d637/deploy.php
+++ html/5de1c9089d637/deploy.php
@@ -1,3 +1,3 @@
 <?php
-exec('cd /home/maitakajp/www/occuptme.maitakajp.com; git pull', $op, $rv);
-exec('cp /home/maitakajp/www/occuptme.maitakajp.com/html/.htaccess.product /home/maitakajp/www/occuptme.maitakajp.com/html/.htaccess');
+exec('cd /home/maitakajp/www/occupyme.maitakajp.com; git pull', $op, $rv);
+exec('cp /home/maitakajp/www/occupyme.maitakajp.com/html/.htaccess.product /home/maitakajp/www/occupyme.maitakajp.com/html/.htaccess');

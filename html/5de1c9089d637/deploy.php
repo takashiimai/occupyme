9404c1c047f04c7df0905d0e01eb5d53014d3239
Takashi imai <takashi.imai.jp@gmail.com>
2020-01-13 13:47:44 +0900

    deploy

--- html/5de1c9089d637/deploy.php
+++ html/5de1c9089d637/deploy.php
@@ -1,3 +1,3 @@
 <?php
 exec('cd /home/maitakajp/www/occupyme.maitakajp.com; git pull', $op, $rv);
-exec('cp /home/maitakajp/www/occupyme.maitakajp.com/html/.htaccess.product /home/maitakajp/www/occupyme.maitakajp.com/html/.htaccess');
+exec('cp /home/maitakajp/www/occupyme.maitakajp.com/html/.htaccess.production /home/maitakajp/www/occupyme.maitakajp.com/html/.htaccess');

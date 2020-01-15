f7f63a0b236dec47419e868e58b931c27ceb3bcd
Takashi imai <takashi.imai.jp@gmail.com>
2020-01-15 22:43:11 +0900

    a

--- application/views/front/ad/adsense_1616161740.php
+++ application/views/front/ad/adsense_1616161740.php
@@ -1,10 +1,10 @@
 <?php if (ENVIRONMENT == 'production') : ?>
 <ins class="adsbygoogle"
      style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-ep+7v-3d-d1+102"
      data-ad-client="ca-pub-7141035308268908"
-     data-ad-slot="8172997086"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
+     data-ad-slot="1616161740"></ins>
 <?php else : ?>
-広告 adsense_no1
+広告 adsense_1616161740
 <?php endif; ?>
--- application/views/front/layout_header_view.php
+++ application/views/front/layout_header_view.php
@@ -3,7 +3,7 @@
         <div class="col-1 text-left px-0"><a href="/top"><img class="img-fluid" src="/images/occuptme.png"></a></div>
         <div class="offset-2"></div>
         <div class="col-6 text-center" style="max-height: 150px !important;">
-<?php include "ad/adsense_no1.php"; ?>
+<?php include "ad/adsense_1616161740.php"; ?>
         </div>
         <div class="offset-2"></div>
         <div class="col-1 text-right px-0"><a class="btn btn-primary rounded-0" href="/member/login"><?= isset($login) && $login ? 'マイページ' :  'ログイン'?></a></div>
